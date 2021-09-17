920de9484f3b4907f3a8c22c424ff8697ed7496b
sahilbakoru <sahilbakoru1999@gmail.com>
2021-09-17 18:18:16 +0530

    commit

--- controller/product.js
+++ controller/product.js
@@ -4,8 +4,22 @@ const fs= require('fs')
 const{errorHandler}= require('../helpers/dbErrorHandler')
 const Product = require('../models/product')
 
+exports.productById=(req,res,next,Id)=>{
+    Product.findById(Id).exec((err,product)=>{
+        if(err||!product){
+            return res.status(400).json({ 
+                error:'Product not found'
+            })
+        }
+        req.product=product
+        next()
+    })
+}
 
-
+exports.read=(req,res)=>{ 
+    req.product.photo=undefined
+    return res.json(req.product)
+}
 
 exports.create= (req,res)=>{ 
     const form = new formidable.IncomingForm()
@@ -40,6 +54,85 @@ exports.create= (req,res)=>{
             }
 
         const product = new Product(fields)
+  // 1kb= 1000
+  //1MB= 1000000
+        if(files.photo){
+
+           //console.log("FILES PHOTO:",files.photo)
+           if(files.photo.size>1000000){
+               return res.status(400).json({
+                   error: "File must be less then 1 mb in size."
+               })
+           }
+            product.photo.data = fs.readFileSync(files.photo.path)
+            product.photo.contentType=files.photo.type
+        }
+        product.save((err,result)=>{
+            if(err) {
+                console.log("Error3 ");
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json({result})
+        })
+
+
+    })
+
+}
+
+exports.remove = (req,res)=> { 
+    let product = req.product
+    product.remove((err,deletedProduct)=>{
+        if(err){
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        }
+        res.json({
+         message: "Product deleted "
+        })
+    })
+}
+
+
+exports.update = (req,res)=>{ 
+    const form = new formidable.IncomingForm()
+    form.keepExtensions = true
+    form.parse(req,(err,fields,files)=>{ 
+        if(err) { 
+           
+            return res.status(400).json({ 
+                error:'Image could not be uploaded'
+            })
+        }
+
+        //check for all fields 
+        const{name,
+             description,
+             price,
+             category,
+             subcategory,
+             quantity,
+             shipping}= fields
+
+             if(!name||
+                !description||
+                !price||
+                !category||
+                !subcategory||
+                !quantity||
+                !shipping){
+                return res.status(400).json({
+                    error:"all fields are required"
+                })
+            }
+
+        let product = req.product
+         product=_.extend(product, fields)   
+
+
   // 1kb= 1000
   //1MB= 1000000
         if(files.photo){
--- routes/product.js
+++ routes/product.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router()
 
 
-const {create} = require("../controller/product")
+const {create,productById ,read,remove,update } = require("../controller/product")
 const {requireSignin,isAuth,isAdmin } = require("../controller/auth")
 const {userById} = require("../controller/user")
 
-
+router.get("/product/:productId",read)
 router.post("/product/create/:userId", requireSignin, isAuth,isAdmin, create)
-
+router.delete("/product/:productId/:userId",requireSignin, isAuth,isAdmin,remove)
+router.put("/product/:productId/:userId",requireSignin, isAuth,isAdmin,update)
 
 router.param('userId',userById)
+router.param('productId',productById)
 module.exports = router
