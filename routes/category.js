9ebf171713d1058921d224338f40f573e2e6c8e3
sahilbakoru <sahilbakoru1999@gmail.com>
2021-09-23 16:22:20 +0530

    commit

--- routes/category.js
+++ routes/category.js
@@ -2,12 +2,19 @@ const express = require('express');
 const router = express.Router()
 
 
-const {create,categoryById,read} = require("../controller/category")
+const {create,categoryById,read,update,remove ,list} = require("../controller/category")
 const {requireSignin,isAuth,isAdmin } = require("../controller/auth")
-const {userById} = require("../controller/user")
+const {userById} = require("../controller/user");
+const { remove } = require('lodash');
+
+
+
 
 router.get("/category/:categoryId", read)
 router.post("/category/create/:userId", requireSignin, isAuth,isAdmin, create)
+router.put("/category/:categoryId/:userId", requireSignin, isAuth,isAdmin, update)
+router.delete("/category/:categoryId/:userId", requireSignin, isAuth,isAdmin, remove)
+router.get("/categories", list)
 
 router.param('categoryId',categoryById)
 router.param('userId',userById)
