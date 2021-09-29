9f0beb751935d42b475e4dd69b8d0a63294067bc
sahilbakoru <sahilbakoru1999@gmail.com>
2021-09-29 22:32:53 +0530

    search_problem

--- routes/product.js
+++ routes/product.js
@@ -6,10 +6,13 @@ const {
     
     create,
     productById ,
-    read,remove,
-    update,list,
+    read,
+    remove,
+    update,
+    list,
     listRelated,
-    listCategories 
+    listCategories,
+    listBySearch 
 
 } = require("../controller/product")
 const {requireSignin,isAuth,isAdmin } = require("../controller/auth")
@@ -25,6 +28,7 @@ router.put("/product/:productId/:userId",requireSignin, isAuth,isAdmin,update)
 router.get("/products",list)
 router.get("/products/related/:productId/",listRelated)
 router.get("/products/categories",listCategories)
+router.post("/products/by/search",listBySearch)
 
 router.param('userId',userById)
 router.param('productId',productById)
